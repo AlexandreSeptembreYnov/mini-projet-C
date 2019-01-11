78d05cbf328df2559a227223d9b22f831abf3cd9
KirihimeNatsuki <enzo.lemarchand@ynov.com>
2019-01-11 17:27:43 +0100

    Ajout debut fonctions

---
+++ Mots_cache/fonctions_verif_mot.cpp
@@ -0,0 +1,40 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+void AddCharDebut()
+{
+
+}
+
+
+void AddCharFin()
+{
+
+}
+
+void RemoveCharDebut()
+{
+
+}
+
+void RemoveCharFin()
+{
+
+}
+
+void VerifMot()
+{
+
+}
+
+
+
+
+
+
+
+
+void VerifSortieTab()
+{
+
+}
