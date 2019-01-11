755994cb5ace1cf35ea187bc81a035891b629497
QuentinNEP <tinoulegamer@gmail.com>
2019-01-11 16:58:55 +0100

    ajouts fonctions

--- Mots_cache/fonctions.cpp
+++ Mots_cache/fonctions.cpp
@@ -21,3 +21,43 @@ int PlacerLettre(){
 int Annuler(){
 
 };
+
+
+
+
+
+int HorizontalDroit(){
+
+};
+
+int HorizontalInverse(){
+
+};
+
+int VerticaleDroit(){
+
+};
+
+int VerticaleInverse(){
+
+};
+
+int DiagonaleDroiteDroit(){
+
+};
+
+int DiagonaleDroiteInverse(){
+
+};
+
+int DiagonaleGaucheDroit(){
+
+};
+
+int DiagonaleGaucheInverse(){
+
+};
+
+void error(){
+
+};
