35ac087c4839baede349b4f3f22dcf4d4a5095b9
KirihimeNatsuki <enzo.lemarchand@ynov.com>
2019-01-11 17:55:40 +0100

    Ajout struct

--- Mots_cache/fonctions_verif_mot.cpp
+++ Mots_cache/fonctions_verif_mot.cpp
@@ -1,28 +1,48 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-void AddCharDebut()
+enum direction {Horizontal=1,Vertical=2,DiagonnalHautDroit=3,DiagonnalHautGauche=4};
+enum sens {Droit=0, Inverse=1};
+typedef struct mot
+{
+    char* labbel;
+    int taille;
+    int xDebut;
+    int yDebut;
+    sens Sens;
+    direction Direction;
+    mot *next;
+} Mot;
+typedef struct Liste Liste;
+struct Liste
+{
+    Mot *head;
+}
+
+void AddCharDebut(char* str,char c)
+{
+    while (xdebut <= 8 && ydebut <= 8) {
+
+    }
+}
+
+
+void AddCharFin(char* str,char c)
 {
 
 }
 
-
-void AddCharFin()
+void RemoveCharDebut(char* str)
 {
 
 }
 
-void RemoveCharDebut()
+void RemoveCharFin(char* str)
 {
 
 }
 
-void RemoveCharFin()
-{
-
-}
-
-void VerifMot()
+void VerifMot(char* str,Liste liste)
 {
 
 }
