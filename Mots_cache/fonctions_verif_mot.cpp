5ce6402ff30a752776d8475ffdc9ddd5efc85c1c
Alexandre Septembre <alexandre.septembre@ynov.com>
2019-01-22 19:46:18 +0100

    1rs debug

--- Mots_cache/fonctions_verif_mot.cpp
+++ Mots_cache/fonctions_verif_mot.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#define TAILLE      8
 
 enum direction {Horizontal=1,Vertical=2,DiagonnalHautDroit=3,DiagonnalHautGauche=4};
 enum sens {Droit=0, Inverse=1};
@@ -17,19 +18,19 @@ typedef struct Liste Liste;
 struct Liste
 {
     Mot *head;
+};
+
+char *AddCharDebut(char *str, char c)
+{
+    str = c+str;
+    return str;
 }
 
-void AddCharDebut(char* str,char c)
+
+char *AddCharFin(char* str,char c)
 {
-    while (xdebut <= 8 && ydebut <= 8) {
-
-    }
-}
-
-
-void AddCharFin(char* str,char c)
-{
-
+    str = str+c;
+    return str;
 }
 
 void RemoveCharDebut(char* str)
@@ -54,7 +55,47 @@ void VerifMot(char* str,Liste liste)
 
 
 
-void VerifSortieTab()
+void VerifSortieTab(Mot *e)
 {
+    int error = 1;
+    while (error){
+        switch (e->Direction) {
+        case 1:
+        {
+            if ((e->xDebut + e->taille) >= TAILLE || (e->xDebut + e->taille) <= 0)
+            {
+                //random xDebut
+            }
+            break;
+        }
+        case 2 :{
+            if ((e->yDebut + e->taille) >= TAILLE || (e->yDebut + e->taille) <= 0)
+            {
+                //random yDebut
+            }
+            break;
+        }
+        case 3 :{
+            if ((e->yDebut + e->taille) >= TAILLE || (e->yDebut + e->taille) <= 0 || (e->xDebut + e->taille) >= TAILLE || (e->xDebut + e->taille) <= 0)
+            {
+                //random yDebut
+                //random xDebut
+            }
+            break;
+        }
+        case 4 :{
+            if ((e->yDebut + e->taille) >= TAILLE || (e->yDebut + e->taille) <= 0 || (e->xDebut + e->taille) >= TAILLE || (e->xDebut + e->taille) <= 0)
+            {
+                //random yDebut
+                //random xDebut
+            }
+            break;
+        }
+        default:
+        {
+            error = 0;
+            break;
+        }
+    }
 
 }
